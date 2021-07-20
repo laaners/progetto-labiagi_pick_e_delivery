392fdb3ddb20fe449cbdcba3fc58e99d984a52ad
hu.1839472 <hu.1839472@studenti.uniroma1.it>
2021-07-20 12:15:48 +0200

    test push

--- pushall.sh
+++ pushall.sh
@@ -2,8 +2,11 @@
 if [ $# -eq 0 ]; then 
 	echo "No argomenti"
 else
-	echo $1	
+	echo $1
+	echo git add -A
 	git add -A
-	git commit -m $1
+	echo git commit -m "$1"
+	git commit -m "$1"
+	echo git push
 	git push
 fi
